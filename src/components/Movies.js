a6eaa731be1fbd2112a8713b69785d2426bc50d9
Felipe Mano <manofgpa@gmail.com>
2021-08-24 07:21:24 -0300

    Add use effect

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,34 +7,64 @@ const MoviesContainer = () => {
 
     const tmdbApiUri = 'https://api.themoviedb.org/3/search/movie?api_key=479b26e5222f9ef3fac0b4d50717c56b&query='
 
-    const [movies, setMovies] = useState('')
-    const [search, setSearch] = useState(1)
-    const [submit, setSubmit] = useState(1)
+    const [moviesData, setMovies] = useState([])
+    const [search, setSearch] = useState('')
+    const [submit, setSubmit] = useState('')
 
     const handleClick = () => {
-        console.log(movies);
         setSubmit(search)
     }
 
     useEffect(() => {
-        axios
-            .get(tmdbApiUri + search)
-            .then(res => {
-                setMovies(res.data.results)
+        if (submit) {
+            axios
+                .get(tmdbApiUri + submit)
+                .then(res => {
+                    setMovies(res.data.results)
 
-            })
-            .catch(err => {
-                console.log(err)
-            })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
     }, [submit])
 
+    useEffect(() => {
+        moviesData.forEach(movie => {
+            // console.log(movie);
+        })
+    }, [moviesData])
+
+    const MoviesList = () => {
+        return (
+            <section>
+                {
+                    moviesData.map(movie => {
+                        return (
+                            <Movie movie={movie} key={movie.id} />
+                        )
+                    })
+                }
+            </section>
+        )
+    }
+
+    const Movie = ({ original_title, id }) => {
+        return (
+            <article className='Movie' >
+                <h1>{original_title}</h1>
+            </article>
+        )
+    }
 
     return (
-        < Search >
-            <input className="form-control" onChange={e => setSearch(e.target.value)} type="text" name="movie" value={search} placeholder='Search for a movie.' />
-            <Button type="button" onClick={handleClick} value="Search" name="search" />
-            <p>{movies.original_title}</p>
-        </Search >
+        <>
+            < Search >
+                <input className="form-control" onChange={e => setSearch(e.target.value)} type="text" name="movie" value={search} placeholder='Search for a movie.' />
+                <Button type="button" onClick={handleClick} value="Search" name="search" />
+            </Search >
+            <MoviesList />
+        </>
     )
 }
 
@@ -46,6 +76,7 @@ const Search = styled.div`
     margin: 0 auto;
     width: 40%;
     display: flex;
+    color: white;
 `
 
 const Button = styled.input`
